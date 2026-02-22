668a4f702990388e69f4d46ae2c719bce77477e2
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:58:09 +0500

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  name: 'gpt',
+  description: 'AI reply command',
+  execute: async(msg, args, sock) => {
+    const reply = await require('../utils/functions').getAIReply(args.join(' '));
+    await sock.sendMessage(msg.key.remoteJid, { text: reply }, { quoted: msg });
+  }
+};
