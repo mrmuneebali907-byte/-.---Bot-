28d17a327255a0e53652852b7b149ff369e1bd69
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:59:02 +0500

    Add TicTacToe game module with execute function

--- games.js
+++ games.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  name: 'tictactoe',
+  description: 'Start TicTacToe game',
+  execute: async(msg, args, sock) => {
+    await sock.sendMessage(msg.key.remoteJid, { text: 'TicTacToe command placeholder' }, { quoted: msg });
+  }
+};
