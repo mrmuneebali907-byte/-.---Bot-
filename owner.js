022da69cf48172197d62c87ab44ed8b31252bcb0
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:57:24 +0500

    Add owner command module with placeholder response

--- owner.js
+++ owner.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  name: 'mode',
+  description: 'Set bot mode',
+  execute: async(msg, args, sock) => {
+    await sock.sendMessage(msg.key.remoteJid, { text: 'Owner command placeholder' }, { quoted: msg });
+  }
+};
