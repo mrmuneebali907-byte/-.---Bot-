fa0c0f90d77255eb45c1fde64c04b58d366849ae
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:52:02 +0500

    Set up main bot loader with command and event handlers
    Initialize bot loader and dynamically load commands and events.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// Main bot loader
+const { default: makeWASocket, useSingleFileAuthState } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const { botName, prefix, ownerName, ownerNumber } = require('./config');
 
+// Load commands dynamically
+global.commands = new Map();
+fs.readdirSync('./commands').forEach(file => {
+  if(file.endsWith('.js')){
+    const command = require(`./commands/${file}`);
+    global.commands.set(command.name, command);
+  }
+});
+
+// Load events
+require('./events/message')();
+require('./events/group')();
+
+console.log(`${botName} v3.0.5 loaded by ${ownerName}`);
