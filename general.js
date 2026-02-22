42b300d7dd91cf144f2fedaad12865b7f1629972
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:55:20 +0500

    Add ping command to general.js

--- general.js
+++ general.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  name: 'ping',
+  description: 'Ping command',
+  execute: async(msg, args, sock) => {
+    await sock.sendMessage(msg.key.remoteJid, { text: 'Pong ✅' }, { quoted: msg });
+  }
+};
