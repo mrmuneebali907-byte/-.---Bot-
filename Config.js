57c1234d58701f7bafe7752125eaf9bd779b8fa0
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:53:45 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  botName: 'MrMuneebAli',
+  ownerName: '𝑴𝑴𝒓.𝑴𝒖𝒏𝒆𝒆𝒃𝑨𝒍𝒊',
+  prefix: '.',
+  ownerNumber: '923XXXXXXXXX', // WhatsApp number
+  aiApiKey: '',                // Free AI API key
+  bannerApiKey: ''             // Optional: for banner image
+};
