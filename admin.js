7a9bb2b74f7a3f378f906f2e9cd9628c73e3d44c
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 09:56:30 +0500

    Add ban command to admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  name: 'ban',
+  description: 'Ban a user in group',
+  execute: async(msg, args, sock) => {
+    await sock.sendMessage(msg.key.remoteJid, { text: 'Ban command placeholder' }, { quoted: msg });
+  }
+};
