3f902852ba36c580c92607316f5a87295dc167cb
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-02-22 10:00:41 +0500

    Add sticker command to image.js

--- image.js
+++ image.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  name: 'sticker',
+  description: 'Convert image to sticker',
+  execute: async(msg, args, sock) => {
+    await sock.sendMessage(msg.key.remoteJid, { text: 'Image/Sticker command placeholder' }, { quoted: msg });
+  }
+};
