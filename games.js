b79f171c4930851ccf87e05cc587554525a887ea
mrmuneebali907-byte <mrmuneebali907@gmail.com>
2026-03-22 23:05:33 +0500

    Delete games.js

--- games.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'tictactoe',
-  description: 'Start TicTacToe game',
-  execute: async(msg, args, sock) => {
-    await sock.sendMessage(msg.key.remoteJid, { text: 'TicTacToe command placeholder' }, { quoted: msg });
-  }
-};
